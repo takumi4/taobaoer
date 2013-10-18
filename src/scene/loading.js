7bb48902fba00e4fcb247d59203740266b637e8c
檀汐 <lichao.lc@alibaba-inc.com>
2013-10-18 18:14:46 +0800

    sprite

--- src/scene/loading.js
+++ src/scene/loading.js
@@ -2,8 +2,8 @@
  * @scene Loading
  */
 Crafty.scene('Loading', function(){
-    Crafty.load(['assets/cc.png'], function(){
-        Crafty.sprite(16, 'assets/cc.png', {
+    Crafty.load(['assets/sprite.png'], function(){
+        Crafty.sprite(16, 'assets/sprite.png', {
             spr_tree: [0,0],
             spr_flower: [1, 0],
             spr_taobao: [2, 0]
